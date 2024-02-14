55bc49841f8accc21a4771824eb67ce2429cbad9
muhdamirkhan <kamir6132@gmail.com>
2024-02-14 21:33:09 +0500

    files pushed

--- chicago_deal_vault.py
+++ chicago_deal_vault.py
@@ -1,95 +1,7 @@
 import pandas as pd
-from mapbox import Geocoder
 from math import radians, cos, sin, asin, sqrt
 
-def process():
-    df = pd.read_excel('cdv.xlsx',sheet_name='Data',skiprows=11)
-
-    df_criteria_chicago = pd.read_excel('ZIA Buying Criteria - Chicagoland Zips (1).xlsx',sheet_name='Chicago')
-    df_criteria_suburbs = pd.read_excel('ZIA Buying Criteria - Chicagoland Zips (1).xlsx',sheet_name='Cook Suburbs')
-    df_criteria_dupage = pd.read_excel('ZIA Buying Criteria - Chicagoland Zips (1).xlsx',sheet_name='DuPage County')
-
-    df_criteria_chicago.drop(columns=['Unnamed: 0'],inplace=True)
-    df_criteria_suburbs.drop(columns=['Unnamed: 0'],inplace=True)
-    df_criteria_dupage.drop(columns=['Unnamed: 0'],inplace=True)
-
-    # to deal with values 12345/78969 in Zip column of Duage County data
-    df_criteria_dupage['Zip'] = df_criteria_dupage['Zip'].astype(str).apply(lambda x: x.split('/') if '/' in x else [x])
-    df_criteria_dupage = df_criteria_dupage.explode('Zip')
-    df_criteria_dupage['Zip'] = df_criteria_dupage['Zip'].astype(int)
-
-    # Transform Balance Columns to Currency Form
-    def format_balance(balance):
-        return f"${balance:,.2f}"
-
-    # Applying the formatting function
-    df['BALANCE_DUE'] = df['BALANCE_DUE'].apply(format_balance)
-    df['ZESTIMATE'] = df['ZESTIMATE'].apply(format_balance)
-    df['Equity'] = df['Equity'].apply(format_balance)
-    df['ORIGINAL_AMOUNT'] = df['ORIGINAL_AMOUNT'].apply(format_balance)
-
-    df = df.sort_values(by=['CITY','ZIP']).reset_index().drop(columns=['index'])
-
-    df_chicago = df[df['CITY']=='Chicago'].reset_index().drop(columns=['index'])
-    df_suburbs = df[df['CITY']!='Chicago'].reset_index().drop(columns=['index'])
-
-
-    df_criteria_suburbs = df_criteria_suburbs[df_criteria_suburbs['Yes/No']=='Yes']
-
-    df_merged_suburbs = df_criteria_suburbs[['City']].merge(df_suburbs,left_on=df_criteria_suburbs['City'].str.lower(), \
-                            right_on=df_suburbs['CITY'].str.lower())
-    df_merged_suburbs.drop(columns=['key_0','City'],inplace=True)
-    df_merged_suburbs = df_merged_suburbs.sort_values(by=['CITY'])
-
-    df_criteria_chicago = df_criteria_chicago[df_criteria_chicago['Yes/No']=='Yes']
-
-    df_merged_chicago = df_criteria_chicago[['Zip']].merge(df_chicago,left_on=['Zip'],right_on=['ZIP'])
-    df_merged_chicago.drop(columns=['Zip'],inplace=True)
-    df_merged_chicago = df_merged_chicago.sort_values(by=['ZIP'])
-
-    df_merged_dupage = df[df['ZIP'].isin(df_criteria_dupage['Zip'].unique())]
-
-    df_merged = df_merged_chicago.append(df_merged_dupage)
-    df_merged = df_merged.append(df_criteria_suburbs)
-    df_removed = df[~df['ZIP'].isin(df_merged['ZIP'])]
-
-    df_removed.to_csv('removed.csv',index=False)
-
-    df_merged_chicago['Location'] = 'Chicago'
-    df_merged_dupage['Location'] = 'DuPage'
-    df_merged_suburbs['Location'] = 'Suburbs'
-
-    df_clean_full = df_merged_chicago.append(df_merged_dupage)
-    df_clean_full = df_clean_full.append(df_merged_suburbs)
-    df_clean_full['ZIP'] = df_clean_full['ZIP'].astype('str')
-    df_clean_full['COMPLETE_ADDRESS'] = df_clean_full['ADDRESS'] +' ' + df_clean_full['CITY'] +', USA '+ df_clean_full['ZIP']
-
-
-    geocoder = Geocoder(access_token="pk.eyJ1IjoiYWsxMzUyIiwiYSI6ImNscmZzeXY3MDA4MG4yam8yajJmaTYxb24ifQ.ZUeGaita89-L-N_usIhvIg")
-    response = geocoder.forward('847 W MONROE ST UNIT 1S Chicago, USA 60607')
-
-
-    # Initialize the Mapbox Geocoder with your access token
-    access_token = "pk.eyJ1IjoiYWsxMzUyIiwiYSI6ImNscmZzeXY3MDA4MG4yam8yajJmaTYxb24ifQ.ZUeGaita89-L-N_usIhvIg"  # Replace with your Mapbox access token
-    geocoder = Geocoder(access_token=access_token)
-
-    # Function to geocode an address
-    def geocode_address(address):
-        print(address)
-        try:
-            response = geocoder.forward(address)
-            # Get the first result (highest relevance)
-            coords = str(response.json()['features'][0]['center']).replace(']', '').replace('[', '').split(',')
-            return coords[0], coords[1]  # lat, lon
-        except IndexError:
-            # Return NaN if no result is found
-            return float('nan'), float('nan')
-
-    # Apply the function to the COMPLETE_ADDRESS column
-    df_clean_full['lat'], df_clean_full['lon'] = zip(*df_clean_full['COMPLETE_ADDRESS'].apply(geocode_address))
-
-
-    def haversine(lon1, lat1, lon2, lat2):
+def haversine(lon1, lat1, lon2, lat2):
         """
         Calculate the great circle distance in kilometers between two points 
         on the earth (specified in decimal degrees)
@@ -105,20 +17,19 @@ def process():
         r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
         return c * r
 
-    df_clean_full['distance_from_canfield'] = 0.0
-    df_clean_full['distance_from_ca_ave'] = 0.0
+def process(df_clean_full,point_lat,point_lon):
+    df_clean_full['distance_from_point'] = 0.0
     df_clean_full['lat'] = df_clean_full['lat'].astype('float')
     df_clean_full['lon'] = df_clean_full['lon'].astype('float')
 
-    canfield_lat_lon = [41.9944459469234, -87.821749488142]
-    ca_ave_lat_lon = [41.91362381711367, -87.69729184581809]
+    canfield_lat_lon = [point_lat, point_lon]
 
-    df_clean_full['distance_from_canfield'] = df_clean_full.apply(lambda row: haversine(row['lat'], 
-                                                                row['lon'],
+    df_clean_full['distance_from_point'] = df_clean_full.apply(lambda row: haversine(row['lon'], 
+                                                                row['lat'],
                                                                 canfield_lat_lon[1],
                                                                 canfield_lat_lon[0]), axis=1)
-    df_clean_full['distance_from_ca_ave'] = df_clean_full.apply(lambda row: haversine(row['lat'], 
-                                                                row['lon'],
-                                                                ca_ave_lat_lon[1],
-                                                                ca_ave_lat_lon[0]), axis=1)
-    df_clean_full.to_csv('df_full_clean.csv',index=False)
+    
+    # km to miles
+    df_clean_full['distance_from_point'] = df_clean_full['distance_from_point'] * 0.621371
+    df_clean_full.to_csv('df_filtered.csv',index=False)
+    return df_clean_full
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,146 +1,110 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
+from chicago_deal_vault import process
 
-
+st.set_page_config(page_title="Chicago Deal Vault Data", layout="wide")
+st.header('Chicago PreForeClosure Data')
+# function to send the email
 def send_email():
      st.sidebar.text("Email Sent \U0001F603")
-# Load the augmented CSV file
+
+# Load CSV file
 df = pd.read_csv('df_full_clean.csv')
 df_closed_deals = pd.read_csv('closed_deals.csv')
-df_closed_deals = df_closed_deals[(df_closed_deals['Address']!= '6231 N Canfield Ave') & 
-                                  (df_closed_deals['Address']!= '1746 N California Ave')]
-
+df['FILING_DATE_FORECLOSURE'] = pd.to_datetime(df['FILING_DATE_FORECLOSURE']).dt.date
 # User Input to choose proximity distance
-distance_input = st.sidebar.number_input('Enter distace (proximity) in miles', min_value=1, value=5, step=1)
+distance_input = st.sidebar.number_input('Enter distance (proximity) in miles', min_value=1, value=5, step=1)
 
-# User Input to select the County
-location = st.sidebar.selectbox(
-    'Please select a County',
-    ('Chicago', 'Suburbs', 'DuPage'))
+start_date = st.sidebar.date_input('Please select start date')                              
+end_date = st.sidebar.date_input('Please select end date')
 
-df = df[df['Location']==location]
+
 
 if st.sidebar.button('Send Email'):
     send_email()
 else:
     print('')
-
-
-# KM to Miles
-df['distance_from_canfield'] = df['distance_from_canfield'] * 1.6
-df['distance_from_ca_ave'] = df['distance_from_ca_ave'] * 1.6
-
-# Define criteria for highlighting points
-close_to_canfield = df['distance_from_canfield'] <= distance_input
-close_to_ca_ave = df['distance_from_ca_ave'] <= distance_input
-
-# Define the two specific locations
-canfield_lat_lon = [41.9944459469234, -87.821749488142]
-ca_ave_lat_lon = [41.91362381711367, -87.69729184581809]
+# User Input to select the County
+location = st.sidebar.selectbox(
+    'Please select a County',
+    ('Chicago', 'Suburbs', 'DuPage'))
+df = df[df['Location']==location]
+st.sidebar.info(f"Date is available from {df['FILING_DATE_FORECLOSURE'].min()} to {df['FILING_DATE_FORECLOSURE'].max()}")
+df = df[(df['FILING_DATE_FORECLOSURE'] >= start_date) & (df['FILING_DATE_FORECLOSURE'] <= end_date)]
+if len(df) < 1:
+    st.sidebar.info('No data found for the dates selected')
+else:
+    df = df[df['Location']==location]
+    
+    df = df[(df['FILING_DATE_FORECLOSURE'] >= start_date) & (df['FILING_DATE_FORECLOSURE'] <= end_date)]
+    first_location = st.selectbox(f'Select an Address for {location}', df_closed_deals['Address'].unique(),key='First Location')
+    first_location_coords = df_closed_deals[df_closed_deals['Address'] == first_location].reset_index()
+    df_closed_deals = df_closed_deals[df_closed_deals['Address'] != first_location]
+    df_new = process(df,first_location_coords.loc[0,'lat'],first_location_coords.loc[0,'lon'])
+    close_to_point = df_new['distance_from_point'] <= distance_input
+    canfield_lat_lon = [first_location_coords.loc[0,'lat'],first_location_coords.loc[0,'lon']]
 
 # Create figure
-fig = go.Figure()
+    fig = go.Figure()
 
-# Plot Canfield Point on map
-fig.add_trace(go.Scattermapbox(
-    lat=[canfield_lat_lon[0]],
-    lon=[canfield_lat_lon[1]],
-    mode='markers+text',
-    marker=go.scattermapbox.Marker(
-        size=18,
-        color='black',
-        opacity=0.5  # Distinct color for Canfield
-    ),
-    text=['Canfield'],
-    textposition='bottom right',
-    name='Canfield'
-))
-# Add scatter plot for points within 5 miles of Canfield
-fig.add_trace(go.Scattermapbox(
-    lat=df[close_to_canfield]['lat'],
-    lon=df[close_to_canfield]['lon'],
-    mode='markers',
-    marker=go.scattermapbox.Marker(
-        size=10,
-        color='red',  # Color for Canfield proximity
-    ),
-    text=df[close_to_canfield]['FILING_DATE_FORECLOSURE'],
-    name='Close to Canfield'
-))
-
-# Add scatter plot for CA Ave location
-fig.add_trace(go.Scattermapbox(
-    lat=[ca_ave_lat_lon[0]],
-    lon=[ca_ave_lat_lon[1]],
-    mode='markers+text',
-    marker=go.scattermapbox.Marker(
-        size=18,
-        color='purple',  # Distinct color for CA Ave
-    ),
-    text=['CA Ave'],
-    textposition='bottom right',
-    name='CA Ave'
-))
-
-# Add scatter plot for points within 5 km of CA Ave
-fig.add_trace(go.Scattermapbox(
-    lat=df[close_to_ca_ave]['lat'],
-    lon=df[close_to_ca_ave]['lon'],
-    mode='markers',
-    marker=go.scattermapbox.Marker(
-        size=10,
-        color='green',
-        opacity=0.5  # Color for CA Ave proximity
-    ),
-    text=df[close_to_ca_ave]['FILING_DATE_FORECLOSURE'],
-    name='Close to CA Ave'
-))
-
-# Add scatter plot for the remaining points
-fig.add_trace(go.Scattermapbox(
-    lat=df[~(close_to_canfield | close_to_ca_ave)]['lat'],
-    lon=df[~(close_to_canfield | close_to_ca_ave)]['lon'],
-    mode='markers',
-    marker=go.scattermapbox.Marker(
-        size=10,
-        color='blue',  # Default color for other points
-    ),
-    text=df[~(close_to_canfield | close_to_ca_ave)]['FILING_DATE_FORECLOSURE'],
-    name='Out of Proximity'
-))
-
-
-# Add Closed deals to the map
-fig.add_trace(go.Scattermapbox(
-    lat=df_closed_deals['lat'],
-    lon=df_closed_deals['lon'],
-    mode='markers',
-    marker=go.scattermapbox.Marker(
-        size=10,
-        color='orange',  # Default color for closed deals
-    ),
-    text=df_closed_deals['Address'],
-    name='Closed Deals'
-))
-
-# Update layout
-fig.update_layout(
-    autosize=True,
-    height = 700,
-    width = 900,
-    hovermode='closest',
-    mapbox=dict(
-        accesstoken='pk.eyJ1IjoiYWsxMzUyIiwiYSI6ImNscmZzeXY3MDA4MG4yam8yajJmaTYxb24ifQ.ZUeGaita89-L-N_usIhvIg',  # Make sure this is your correct Mapbox Access Token
-        bearing=0,
-        center=dict(
-            lat=df['lat'].mean(),
-            lon=df['lon'].mean()
+    # Plot Canfield Point on map
+    fig.add_trace(go.Scattermapbox(
+        lat=[canfield_lat_lon[0]],
+        lon=[canfield_lat_lon[1]],
+        mode='markers+text',
+        marker=go.scattermapbox.Marker(
+            size=18,
+            color='black',
+            opacity=0.5  # Distinct color for Canfield
         ),
-        pitch=0,
-        zoom=10
-    ),
-)
+        text=first_location_coords['Address'],
+        textposition='bottom right',
+        name=first_location_coords.loc[0,'Address']
+    ))
+    # Add scatter plot for points within 5 miles of Canfield
+    fig.add_trace(go.Scattermapbox(
+        lat=df[close_to_point]['lat'],
+        lon=df[close_to_point]['lon'],
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=10,
+            color='red',  # Color for Canfield proximity
+        ),
+        text="<b>Filing Date:</b> " + df[close_to_point]['FILING_DATE_FORECLOSURE'].astype('str') + "<br>"  + "<b>Address: </b>" +df[close_to_point]['ADDRESS'] ,
+        name=f"Within {distance_input} miles from {first_location_coords.loc[0,'Address']}"
+    ))
 
-# Display the map
-st.plotly_chart(fig)
+    # Add Closed deals to the map
+    fig.add_trace(go.Scattermapbox(
+        lat=df_closed_deals['lat'],
+        lon=df_closed_deals['lon'],
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=10,
+            color='orange',  # Default color for closed deals
+        ),
+        text= "<b>Address: </b>" + df_closed_deals['Address'],
+        name='Closed Deals'
+    ))
+
+    # Update layout
+    fig.update_layout(
+        autosize=True,
+        height = 700,
+        width = 900,
+        hovermode='closest',
+        mapbox=dict(
+            accesstoken='pk.eyJ1IjoiYWsxMzUyIiwiYSI6ImNsc2Q5dmdzdzBnNGsyanRkY3BmOXIydGIifQ.dVj-_EwangGrP_AClR6cEA',  # Make sure this is your correct Mapbox Access Token
+            bearing=0,
+            center=dict(
+                lat=first_location_coords['lat'].mean(),
+                lon=first_location_coords['lon'].mean()
+            ),
+            pitch=0,
+            zoom=10
+        ),
+    )
+
+    # Display the map
+    st.plotly_chart(fig)
