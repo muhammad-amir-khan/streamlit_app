e4bfa3a8e405c7625d88967cb456c5d7c76a6646
muhdamirkhan <kamir6132@gmail.com>
2024-02-16 09:56:51 +0500

    changes made

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ df['FILING_DATE_FORECLOSURE'] = pd.to_datetime(df['FILING_DATE_FORECLOSURE']).dt
 # User Input to choose proximity distance
 distance_input = st.sidebar.number_input('Enter distance (proximity) in miles', min_value=1, value=5, step=1)
 
-start_date = st.sidebar.date_input('Please select start date')                              
-end_date = st.sidebar.date_input('Please select end date')
+start_date = st.sidebar.date_input('Please select start date',value=df['FILING_DATE_FORECLOSURE'].min())                              
+end_date = st.sidebar.date_input('Please select end date',value=df['FILING_DATE_FORECLOSURE'].max())
 
 
 
@@ -43,25 +43,28 @@ else:
     df_closed_deals = df_closed_deals[df_closed_deals['Address'] != first_location]
     df_new = process(df,first_location_coords.loc[0,'lat'],first_location_coords.loc[0,'lon'])
     close_to_point = df_new['distance_from_point'] <= distance_input
-    canfield_lat_lon = [first_location_coords.loc[0,'lat'],first_location_coords.loc[0,'lon']]
+    point_lat_lon = [first_location_coords.loc[0,'lat'],first_location_coords.loc[0,'lon']]
+
+
 
 # Create figure
     fig = go.Figure()
-
-    # Plot Canfield Point on map
+ ########################## User Selected Location Mapping ##########################
+    # Plot user selected Point on map
     fig.add_trace(go.Scattermapbox(
-        lat=[canfield_lat_lon[0]],
-        lon=[canfield_lat_lon[1]],
+        lat=[point_lat_lon[0]],
+        lon=[point_lat_lon[1]],
         mode='markers+text',
         marker=go.scattermapbox.Marker(
             size=18,
-            color='black',
-            opacity=0.5  # Distinct color for Canfield
+            color='green',
+            opacity=0.8  # Distinct color for Canfield
         ),
         text=first_location_coords['Address'],
         textposition='bottom right',
         name=first_location_coords.loc[0,'Address']
     ))
+
     # Add scatter plot for points within 5 miles of Canfield
     fig.add_trace(go.Scattermapbox(
         lat=df[close_to_point]['lat'],
@@ -69,7 +72,7 @@ else:
         mode='markers',
         marker=go.scattermapbox.Marker(
             size=10,
-            color='red',  # Color for Canfield proximity
+            color='green',  # Color for Canfield proximity
         ),
         text="<b>Filing Date:</b> " + df[close_to_point]['FILING_DATE_FORECLOSURE'].astype('str') + "<br>"  + "<b>Address: </b>" +df[close_to_point]['ADDRESS'] ,
         name=f"Within {distance_input} miles from {first_location_coords.loc[0,'Address']}"
@@ -82,7 +85,7 @@ else:
         mode='markers',
         marker=go.scattermapbox.Marker(
             size=10,
-            color='orange',  # Default color for closed deals
+            color='blue',  # Default color for closed deals
         ),
         text= "<b>Address: </b>" + df_closed_deals['Address'],
         name='Closed Deals'
@@ -97,6 +100,7 @@ else:
         mapbox=dict(
             accesstoken='pk.eyJ1IjoiYWsxMzUyIiwiYSI6ImNsc2Q5dmdzdzBnNGsyanRkY3BmOXIydGIifQ.dVj-_EwangGrP_AClR6cEA',  # Make sure this is your correct Mapbox Access Token
             bearing=0,
+            style="satellite-streets",
             center=dict(
                 lat=first_location_coords['lat'].mean(),
                 lon=first_location_coords['lon'].mean()
