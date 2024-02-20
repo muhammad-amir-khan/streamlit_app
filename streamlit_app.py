dbe9c9588657cbb87ab9e17ebec44ce8b8bb6c72
muhammad-amir-khan <111272272+muhammad-amir-khan@users.noreply.github.com>
2024-02-20 21:03:36 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-from chicago_deal_vault import process
+from chicago_deal_vault import process, aggregate_addresses
+from io import StringIO
 
 st.set_page_config(page_title="Chicago Deal Vault Data", layout="wide")
 st.header('Chicago PreForeClosure Data')
@@ -9,28 +10,63 @@ st.header('Chicago PreForeClosure Data')
 def send_email():
      st.sidebar.text("Email Sent \U0001F603")
 
-# Load CSV file
-df = pd.read_csv('df_full_clean.csv')
+
+# Load Files
+df = pd.read_csv('preforeclosure.csv')
+df_probate = pd.read_excel('probate.xlsx',sheet_name='Data',skiprows=11)
 df_closed_deals = pd.read_csv('closed_deals.csv')
+df_auctions = pd.read_excel('auctions.xlsx',sheet_name='Data',skiprows=11)
+df_probate.rename(columns={'Deceased_Address':'ADDRESS'},inplace=True)
+
+################## LIST STACKING STARTS ##############################
+def convert_df_to_csv(df):
+    # Convert DataFrame to CSV
+    output = StringIO()
+    df.to_csv(output, index=False)
+    return output.getvalue()
+
+# Assuming you have a button or mechanism to upload or select dataframes (df1, df2, df3)
+if st.button('Create List Stacking'):
+    # Process addresses (replace df1, df2, df3 with actual DataFrames)
+    result_df = aggregate_addresses(df[['ADDRESS']],df_probate[['ADDRESS']],df_auctions[['ADDRESS']])
+    # Filter out addresses that are in at least two of the files
+    result_df = result_df[result_df['count'] >= 2]
+    result_df.drop(columns={'count'},inplace=True)
+    # Show the filtered DataFrame in the app (optional)
+    st.write('Addresses in at Least Two Files:', result_df)
+
+################### LIST STACKING ENDS ######################################
+    
 df['FILING_DATE_FORECLOSURE'] = pd.to_datetime(df['FILING_DATE_FORECLOSURE']).dt.date
+min_date_formatted = df['FILING_DATE_FORECLOSURE'].min().strftime('%m/%d/%Y')
+max_date_formatted = df['FILING_DATE_FORECLOSURE'].max().strftime('%m/%d/%Y')
+
+# Display the formatted date range in the sidebar
+st.sidebar.info(f"Data is available from {min_date_formatted} to {max_date_formatted}")
+
 # User Input to choose proximity distance
 distance_input = st.sidebar.number_input('Enter distance (proximity) in miles', min_value=1, value=5, step=1)
 
-start_date = st.sidebar.date_input('Please select start date',value=df['FILING_DATE_FORECLOSURE'].min())                              
-end_date = st.sidebar.date_input('Please select end date',value=df['FILING_DATE_FORECLOSURE'].max())
-
-
-
+# date input filters
+start_date = st.sidebar.date_input('Please select start date',
+                                   value=df['FILING_DATE_FORECLOSURE'].min(),
+                                   format="MM/DD/YYYY")                              
+end_date = st.sidebar.date_input('Please select end date',
+                                 value=df['FILING_DATE_FORECLOSURE'].max(),
+                                 format="MM/DD/YYYY")
+# email sending part
 if st.sidebar.button('Send Email'):
     send_email()
 else:
     print('')
+
 # User Input to select the County
 location = st.sidebar.selectbox(
     'Please select a County',
     ('Chicago', 'Suburbs', 'DuPage'))
 df = df[df['Location']==location]
-st.sidebar.info(f"Date is available from {df['FILING_DATE_FORECLOSURE'].min()} to {df['FILING_DATE_FORECLOSURE'].max()}")
+
+
 df = df[(df['FILING_DATE_FORECLOSURE'] >= start_date) & (df['FILING_DATE_FORECLOSURE'] <= end_date)]
 if len(df) < 1:
     st.sidebar.info('No data found for the dates selected')
@@ -78,7 +114,7 @@ else:
         name=f"Within {distance_input} miles from {first_location_coords.loc[0,'Address']}"
     ))
 
-    # Add Closed deals to the map 
+    # Add Closed deals to the map
     fig.add_trace(go.Scattermapbox(
         lat=df_closed_deals['lat'],
         lon=df_closed_deals['lon'],
