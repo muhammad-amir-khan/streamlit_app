4dbe5d8488d6f1642be65c34f16e3ca0a812d8d0
muhammad-amir-khan <111272272+muhammad-amir-khan@users.noreply.github.com>
2024-03-03 22:33:45 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,9 @@ import os, time
 st.set_page_config(page_title="Chicago Deal Vault Data", layout="wide")
 st.header('Chicago Deal Vault Data')
 
-
+def convert_df_to_csv(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
 
 # Load Files
 df = pd.read_csv('pfc.csv')
@@ -39,7 +41,7 @@ if uploaded_file is not None:
         time.sleep(2)
         message.text('')
         df_pfc_new = pd.read_excel('pfc.xlsx',sheet_name='Data',skiprows=11)
-        process_new_data(df_pfc_new,convert_address=True,data='pfc')
+        process_new_data(df_pfc_new,data='pfc')
         
         df = pd.read_csv('pfc.csv')
 
@@ -51,7 +53,7 @@ if uploaded_file is not None:
         message.text('')
         df_auctions_new = pd.read_excel('auct.xlsx',sheet_name='Data',skiprows=11)
         message.text('Processing uploaded data...')
-        process_new_data(df_auctions_new,convert_address=False,data='auct')
+        process_new_data(df_auctions_new,data='auct')
         message.text('All good!')
 
     elif uploaded_file.name.lower() == 'prob.xlsx':
@@ -68,7 +70,7 @@ if uploaded_file is not None:
         if 'Deceased_Zip' in df_probate_new.columns:
             df_probate_new.rename(columns={'Deceased_Zip':'ZIP'},inplace=True)
         message.text('Processing uploaded data...')
-        process_new_data(df_probate_new,convert_address=False,data='prob')
+        process_new_data(df_probate_new,data='prob')
         message.text('All good!')
         
     else:
@@ -85,7 +87,6 @@ if st.button('Create List Stacking'):
     df_pr.rename(columns={'Deceased_Address':'ADDRESS'},inplace=True)
 
     all_data = pd.concat([df_pf,df_pr,df_au],axis=0)
-    all_data.to_csv('all_data.csv')
     # Process addresses (replace df1, df2, df3 with actual DataFrames)
     result_df = aggregate_addresses(df_pf[['ADDRESS']],df_pr[['ADDRESS']],df_au[['ADDRESS']])
     # Filter out addresses that are in at least two of the files
@@ -95,33 +96,75 @@ if st.button('Create List Stacking'):
     result_df = result_df.drop_duplicates(subset=['ADDRESS'],keep='first')
     result_df.drop(columns = ['index','sum','count'],inplace=True)
     result_df.drop_duplicates(subset=['ADDRESS'],keep='first',inplace=True)
-    #result_df.drop(columns={'count'},inplace=True)
+    result_df.reset_index(inplace=True)#
+    result_df.drop(columns=['index'],inplace=True)
+
+    df_au = df_au[df_au['ADDRESS'].isin(list(result_df['ADDRESS']))]
+    df_pr = df_pr[df_pr['ADDRESS'].isin(list(result_df['ADDRESS']))]
+    df_pf = df_pf[df_pf['ADDRESS'].isin(list(result_df['ADDRESS']))]
+    list_stacked_address = pd.concat([df_pf,df_au,df_pr])
+    list_stacked_address.drop_duplicates(subset=['ADDRESS','Type'],keep='first',inplace=True)
+    st.download_button(
+    label="Download list stacking results",
+    data=convert_df_to_csv(list_stacked_address),
+    file_name='list_stacking_export.csv',
+    mime='text/csv',
+    )
+
     # Show the filtered DataFrame in the app (optional)
     st.write('Addresses in at Least Two Files:', result_df)
 ################### LIST STACKING ENDS ######################################
     
-df['FILING_DATE_FORECLOSURE'] = pd.to_datetime(df['FILING_DATE_FORECLOSURE']).dt.date
-min_date_formatted = df['FILING_DATE_FORECLOSURE'].min().strftime('%m/%d/%Y')
-max_date_formatted = df['FILING_DATE_FORECLOSURE'].max().strftime('%m/%d/%Y')
+# Assuming df, df_probate, and df_auctions are your DataFrames
+df['FILING_DATE_FORECLOSURE'] = pd.to_datetime(df['FILING_DATE_FORECLOSURE'])
+df_probate['Filing_Date'] = pd.to_datetime(df_probate['Filing_Date'])
+df_auctions['AUCTION_DATE'] = pd.to_datetime(df_auctions['AUCTION_DATE'])
+
+# Concatenate the Date columns from all three DataFrames without converting to date
+dates = pd.concat([df['FILING_DATE_FORECLOSURE'], df_probate['Filing_Date'], df_auctions['AUCTION_DATE']])
+
+# Now, find the minimum and maximum date
+min_date = dates.min().date()
+max_date = dates.max().date()
+
+
+min_date_formatted = min_date.strftime('%m/%d/%Y')
+max_date_formatted = max_date.strftime('%m/%d/%Y')
 
 # Display the formatted date range in the sidebar
 st.sidebar.info(f"Data is available from {min_date_formatted} to {max_date_formatted}")
 
 # User Input to choose proximity distance
-distance_input = st.sidebar.number_input('Enter distance (proximity) in miles', min_value=1, value=5, step=1)
+distance_input = st.sidebar.number_input('Enter distance (proximity) in miles', min_value=1, value=1, step=1)
 
 # date input filters
-start_date = st.sidebar.date_input('Please select start date',
+start_date = pd.Timestamp(st.sidebar.date_input('Please select start date',
                                    value=df['FILING_DATE_FORECLOSURE'].min(),
-                                   format="MM/DD/YYYY")                              
-end_date = st.sidebar.date_input('Please select end date',
+                                   format="MM/DD/YYYY"))                          
+end_date = pd.Timestamp(st.sidebar.date_input('Please select end date',
                                  value=df['FILING_DATE_FORECLOSURE'].max(),
-                                 format="MM/DD/YYYY")
+                                 format="MM/DD/YYYY"))
 
 # User Input to select the County
 location = st.sidebar.selectbox(
     'Please select a County',
     ('Chicago', 'Suburbs', 'DuPage'))
+
+########## Combine Data for Mapping Starts ##########
+# df['FILING_DATE_FORECLOSURE'] = pd.to_datetime(df['FILING_DATE_FORECLOSURE'])
+# df_probate.rename(columns={'Filing_Date':'FILING_DATE_FORECLOSURE'},inplace=True)
+# df_auctions.rename(columns={'AUCTION_DATE':'FILING_DATE_FORECLOSURE'},inplace=True)
+
+# df.columns = df.columns.str.upper()
+# df_probate.columns = df_probate.columns.str.upper()
+# df_auctions.columns = df_auctions.columns.str.upper()
+
+# df.reset_index(inplace=True)
+# df_probate.reset_index(inplace=True)
+# df_auctions.reset_index(inplace=True)
+
+# df = pd.concat([df,df_probate,df_auctions])
+######### Combine Data for Mapping Ends ##########
 
 df = df[(df['FILING_DATE_FORECLOSURE'] >= start_date) & (df['FILING_DATE_FORECLOSURE'] <= end_date)]
 if len(df) < 1:
@@ -138,9 +181,7 @@ else:
     point_lat_lon = [first_location_coords.loc[0,'lat'],first_location_coords.loc[0,'lon']]
 
     ################ Download Map Data as CSV Starts ##########################################
-    def convert_df_to_csv(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-        return df.to_csv().encode('utf-8')
+    
 
 
     st.download_button(
@@ -163,22 +204,22 @@ else:
         mode='markers+text',
         marker=go.scattermapbox.Marker(
             size=18,
-            color='green',
-            opacity=0.8  # Distinct color for Canfield
+            color='red',
+            opacity=0.9  # Distinct color for User selected location
         ),
         text=first_location_coords['Address'],
         textposition='bottom right',
         name=first_location_coords.loc[0,'Address']
     ))
 
-    # Add scatter plot for points within 5 miles of Canfield
+    # Add scatter plot for points within X User selected location
     fig.add_trace(go.Scattermapbox(
         lat=df[close_to_point]['lat'],
         lon=df[close_to_point]['lon'],
         mode='markers',
         marker=go.scattermapbox.Marker(
             size=10,
-            color='green',  # Color for Canfield proximity
+            color='green',  # Color for User selected location proximity
         ),
         text="<b>Filing Date:</b> " + df[close_to_point]['FILING_DATE_FORECLOSURE'].astype('str') + "<br>"  + "<b>Address: </b>" +df[close_to_point]['ADDRESS'] ,
         name=f"Within {distance_input} miles from {first_location_coords.loc[0,'Address']}"
